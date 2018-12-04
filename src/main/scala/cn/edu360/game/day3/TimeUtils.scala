e6abddedf300400cbea198a5d9c47ec112b211d3
weiyang <179709020@qq.com>
2018-12-04 20:41:10 +0800

    添加 spark Sql 功能；

---
+++ src/main/scala/cn/edu360/game/day3/TimeUtils.scala
@@ -0,0 +1,29 @@
+package cn.edu360.game.day3
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+/**
+  * Created by root on 2016/5/23.
+  */
+object TimeUtils {
+
+  val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  val calendar = Calendar.getInstance()
+
+  //2016-02-01
+  def apply(time: String) = {
+    calendar.setTime(simpleDateFormat.parse(time))
+    calendar.getTimeInMillis
+  }
+
+  //2016-02-01
+  def getCertainDayTime(amount: Int): Long = {
+    calendar.add(Calendar.DATE, amount)
+    val time = calendar.getTimeInMillis
+    calendar.add(Calendar.DATE, -amount)
+    time
+  }
+
+
+}
